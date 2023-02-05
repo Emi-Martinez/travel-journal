26d433b3dd66f107bc11ec1dd829466478684f77
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-02-05 20:44:19 -0300

    App.js is adapted to routing

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import { useSelector, useDispatch } from "react-redux";
-import { fetchLocations } from "./store/locations-actions";
-import { popUpActions } from "./store/popUp-slice";
-import { useEffect } from "react";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
-import Button from "./components/Button"
-import PopUp from "./components/PopUp";
-import Alert from "./components/Alert";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import Singin from "./pages/Singin";
+import TravelJournal from "./pages/TravelJournal";
 import "./style.css"
 
-
-
 export default function App(){
-    const dispatch = useDispatch()
-    const locations = useSelector((state) => state.locations.locations)
-    const popUp = useSelector((state) => state.popUp)
-    const alert = useSelector((state) => state.alert)
-
-    const handleClick = () => {
-        dispatch(popUpActions.togglePopUp("newLocation"))
-    }
-
-    useEffect(()=>{
-        dispatch(fetchLocations())
-    },[dispatch])
-
-    const cards = locations.map((location) => (
-        <Card
-            key={location.id}
-            location = {location}
-        />
-    ))
-
     return(
-        <div>            
-            {popUp.toggle && <PopUp />}
-            {alert.toggle && <Alert message={alert.message} />}
-            <Navbar />            
-            <div className="card-container">
-                {cards}               
-                <Button onClick={handleClick}>
-                    Add location
-                </Button>
-            </div>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<SharedLayout />} >
+                    <Route index element={<Singin />} />
+                    <Route path="traveljournal" element={<TravelJournal />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
