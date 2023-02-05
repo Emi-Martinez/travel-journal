aa4129e61251e826b8d3fa770a8b5e600f14d040
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-02-05 20:49:50 -0300

    pages are added for routing

---
+++ src/pages/TravelJournal.js
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchLocations } from "../store/locations-actions";
+import { popUpActions } from "../store/popUp-slice";
+import { useEffect } from "react";
+import Card from "../components/Card";
+// import Navbar from "../components/Navbar";
+import Button from "../components/Button"
+import PopUp from "../components/PopUp";
+import Alert from "../components/Alert";
+
+export default function TravelJournal(){
+    const dispatch = useDispatch()
+    const locations = useSelector((state) => state.locations.locations)
+    const popUp = useSelector((state) => state.popUp)
+    const alert = useSelector((state) => state.alert)
+
+    const handleClick = () => {
+        dispatch(popUpActions.togglePopUp("newLocation"))
+    }
+
+    useEffect(()=>{
+        dispatch(fetchLocations())
+    },[dispatch])
+
+    const cards = locations.map((location) => (
+        <Card
+            key={location.id}
+            location = {location}
+        />
+    ))
+
+    return(
+        <div>            
+            {popUp.toggle && <PopUp />}
+           {alert.toggle && <Alert message={alert.message} />}          
+            <div className="card-container">
+                {cards}               
+                <Button onClick={handleClick}>
+                    Add location
+                </Button>
+            </div>
+        </div>
+    )
+}
