1a7714266cc00da8194c9ca3d5823d4e582e5a5f
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-07 15:47:20 -0300

    New slices added to store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit"
 import locationsSlice from "./locations-slice"
-import popUpSlice from "./popUp-slice"
+import popUpLoadLocationSlice from "./popUpLoadLocation-slice"
 import locationSlice from "./location-slice"
 import alertSlice from "./alert-slice"
+import userOptionsUISlice from "./userOptionsUI-slice"
+import userSlice from "./user-slice"
+import responsiveSlice from "./responsive-slice"
+import messagePopUpSlice from "./messagePopUp-slice"
 
 const store = configureStore({
     reducer:{
         locations: locationsSlice.reducer,
-        popUp: popUpSlice.reducer,
+        popUpLoadLocation: popUpLoadLocationSlice.reducer,
         location: locationSlice.reducer,
-        alert: alertSlice.reducer
+        alert: alertSlice.reducer,
+        user: userSlice.reducer,
+        userOptionsUI: userOptionsUISlice.reducer,
+        responsive: responsiveSlice.reducer,
+        messagePopUp: messagePopUpSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
