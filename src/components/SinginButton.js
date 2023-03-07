61e1437822aa218a5908c7c9c46fdabe8e17635a
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-07 13:38:05 -0300

    SinginButton.js Modified

--- src/components/SinginButton.js
+++ src/components/SinginButton.js
@@ -1,6 +1,5 @@
-import {ReactComponent as FacebookSVG} from "../icons/facebook.svg"
 import {ReactComponent as GoogleSVG} from "../icons/google.svg"
-import {ReactComponent as GlassesSVG} from "../icons/glasses.svg"
+import {ReactComponent as Gears} from "../icons/gears.svg"
 
 const style = {
     icons: {
@@ -15,16 +14,14 @@ const style = {
 }
 
 const icons = {
-    facebook : <FacebookSVG style={style.icons} />,
     google: <GoogleSVG style={style.icons} />,
-    glasses: <GlassesSVG  style={{...style.icons, width: "30px", marginRight: "5px"}}/>
-
+    gears: <Gears style={{...style.icons, width: "30px", marginRight: "10px"}}/>
 }
 
 export default function SinginButton(props){
 
     return(
-        <button className="btn" >
+        <button className="btn" {...props} >
            { props.variant && icons[props.variant] }
             <span style={style.text} >{props.children}</span>
         </button>
