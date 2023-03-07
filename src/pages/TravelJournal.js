ce369443c4fb4b713aac249ddc508874e1499038
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-07 15:27:16 -0300

    add custom hook and useEffect to TravejJournal.js

--- src/pages/TravelJournal.js
+++ src/pages/TravelJournal.js
@@ -1,26 +1,45 @@
 import { useSelector, useDispatch } from "react-redux";
 import { fetchLocations } from "../store/locations-actions";
-import { popUpActions } from "../store/popUp-slice";
+import {testModeFetchLocations} from "../test/testModeLocations-actions"
+import { popUpLoadLocationActions } from "../store/popUpLoadLocation-slice";
 import { useEffect } from "react";
 import Card from "../components/Card";
-// import Navbar from "../components/Navbar";
 import Button from "../components/Button"
-import PopUp from "../components/PopUp";
+import PopUpLoadLocation from "../components/PopUpLoadLocation";
 import Alert from "../components/Alert";
+import MessagePopUp from "../components/MessagePopUp";
+import { useNavigate } from "react-router-dom"
+import useWindowSize from "../hooks/useWindowSize";
+import NoCardsAction from "../components/NoCardsAction";
 
 export default function TravelJournal(){
+    
+    const navigate = useNavigate()
+    const session = useSelector((state) => state.user.session)
+
     const dispatch = useDispatch()
     const locations = useSelector((state) => state.locations.locations)
-    const popUp = useSelector((state) => state.popUp)
+    const popUpLoadLocation = useSelector((state) => state.popUpLoadLocation)
     const alert = useSelector((state) => state.alert)
+    const messagePopUp = useSelector((state) => state.messagePopUp)
+    useWindowSize(dispatch)    
+    const responsive = useSelector((state) => state.responsive.mobile)
 
     const handleClick = () => {
-        dispatch(popUpActions.togglePopUp("newLocation"))
+        dispatch(popUpLoadLocationActions.togglePopUp("newLocation"))
     }
 
     useEffect(()=>{
-        dispatch(fetchLocations())
-    },[dispatch])
+        if(session === undefined){
+            return navigate('/')
+        }
+        if(session === "test-mode"){
+            dispatch(testModeFetchLocations())
+        }else{
+            dispatch(fetchLocations(session.email))
+        }
+              
+    },[dispatch, navigate, session])
 
     const cards = locations.map((location) => (
         <Card
@@ -28,14 +47,18 @@ export default function TravelJournal(){
             location = {location}
         />
     ))
-
+    
     return(
-        <div>            
-            {popUp.toggle && <PopUp />}
-           {alert.toggle && <Alert message={alert.message} />}          
-            <div className="card-container">
-                {cards}               
-                <Button onClick={handleClick}>
+        <div>
+            {messagePopUp.toggle && <MessagePopUp /> }
+            {popUpLoadLocation.toggle && <PopUpLoadLocation />}
+            {alert.toggle && <Alert message={alert.message} />}          
+            <div className={ responsive ? "card-container-mobile" : "card-container"}>
+                {cards.length !== 0 ? cards : <NoCardsAction /> }               
+                <Button 
+                    onClick={handleClick}
+                    variant={responsive ? "mobile" : undefined}
+                >
                     Add location
                 </Button>
             </div>
