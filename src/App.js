b6622de10ab0818f178b53e30747b913301d1879
Emiliano Martinez <emi.martinez9696@gmail.com>
2023-03-07 13:32:25 -0300

    Added firebase to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { userActions } from "./store/user-slice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
 import Singin from "./pages/Singin";
@@ -5,6 +10,15 @@ import TravelJournal from "./pages/TravelJournal";
 import "./style.css"
 
 export default function App(){
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) =>{
+            if(user){
+                dispatch(userActions.logIn(user))
+            }
+        })
+    }, [dispatch])
+
     return(
         <BrowserRouter>
             <Routes>
